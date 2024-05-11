6a06863dcfb1cf19ec9a5c41745d285527f2450d
Almansur342 <abdullahalmansur2002@gmail.com>
2024-05-11 20:07:32 +0600

    project setup completed

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const app = express();
+
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174'
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.skihu85.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+  const foodCollection = client.db('foodSharing').collection('food');
+
+  app.get('/sixFood', async(req,res)=>{
+    
+  })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res)=>{
+  res.send('hello from food sharing server')
+})
+
+app.listen(port, ()=>{
+  console.log(`server is running on port: ${port}`)
+})
