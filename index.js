c90e4b6e277183825b9926fc5d4822afd1291009
Almansur342 <abdullahalmansur2002@gmail.com>
2024-05-13 22:55:45 +0600

    token validition completed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -15,6 +17,24 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+app.use(cookieParser())
+
+// verify jwt
+const verifyToken = (req,res, next)=>{
+  const token = req.cookies?.token
+  if(!token) return res.status(401).send({message:"unauthorized access"})
+  if(token){
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+      if(err){
+       return res.status(401).send({message:"unauthorized access"})
+      }
+      console.log(decoded)
+      req.user = decoded
+      next()
+    })
+  }
+ 
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.skihu85.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -33,6 +53,30 @@ async function run() {
   const foodCollection = client.db('foodSharing').collection('food');
   const requestCollection = client.db('foodSharing').collection('request');
 
+    // jwt generate
+    app.post('/jwt', async(req,res)=>{
+      const user = req.body
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'365d'
+      })
+      res.cookie('token', token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV=== 'production',
+        sameSite: process.env.NODE_ENV=== 'production'? 'none' : 'strict'
+      }).send({success: true})
+    })
+
+    // clear token after logout
+    app.get('/logout', (req,res)=>{
+      res.clearCookie('token',{
+        httpOnly: true,
+        secure: process.env.NODE_ENV=== 'production',
+        sameSite: process.env.NODE_ENV=== 'production'? 'none' : 'strict',
+        maxAge:0,
+      }).send({success: true})
+    })
+
+
   app.get('/sixFood', async(req,res)=>{
     const cursor = foodCollection.find().limit(6);
     const result = await cursor.toArray();
@@ -46,8 +90,12 @@ async function run() {
     res.send(result)
   });
 
-  app.get('/requestedFood/:email', async(req,res)=>{
-    const email = req.params.email;
+  app.get('/requestedFood/:email', verifyToken, async(req,res)=>{
+    const tokenEmail = req.user.email
+    const email = req.params.email
+    if(tokenEmail !== email){
+      return res.status(403).send({message:"forbidden access"})
+    }
     const query = {requestor: email}
     const results = await requestCollection.find(query).toArray()
     res.send(results)
